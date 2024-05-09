d77f73de6ae06fb4baccb353e760fdcc919cf705
littlegaz <gareth.t.horne@hotmail.co.uk>
2024-05-09 11:15:39 +0100

    employees done

--- classes/Employees.js
+++ classes/Employees.js
@@ -1,4 +1,29 @@
 class Employees {
+    #salary
+    #isHired
+    constructor(name, title, salary, isHired = true){
+        this.name = name
+        this.title = title
+        this.#salary = salary
+        this.#isHired = isHired
+    }
+
+    getSalary(){
+        return this.#salary
+    }
+
+    setSalary(amount){
+        this.#salary = amount
+    }
+
+    getStatus(){
+        return this.#isHired
+    }
+
+    setstatus(command){
+        if (command === "hire"){this.#isHired = true}
+        else (command === "fire");this.#isHired = false
+    }
 
 
 
