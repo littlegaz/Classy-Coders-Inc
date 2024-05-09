da1e865d99c4ef1d1671321a514a13a7a83a4c00
littlegaz <gareth.t.horne@hotmail.co.uk>
2024-05-09 11:28:44 +0100

    manager done

--- classes/Manager.js
+++ classes/Manager.js
@@ -1,7 +1,21 @@
-class Manager {
+const { Employees } = require("./Employees");
 
+class Manager extends Employees{
+    #employeesManaged
+    constructor(department, employeesManaged = []){
+        this.department = department
+        this.#employeesManaged = employeesManaged
+    }
 
+    getEmployeesManaged(){
+        return this.#employeesManaged
+    }
 
+    setEmployeesManaged(employee){
+        if (employee === undefined){throw new Error("You must include and employee")};
+        else if (employee === null){throw new Error("You must include and employee")};
+        else this.#employeesManaged.push(employee)
+    }
     
 }
 
