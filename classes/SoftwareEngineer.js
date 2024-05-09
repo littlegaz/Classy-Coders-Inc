90487cfae3f3982be858eada8ac6d8753f7d4d1c
littlegaz <gareth.t.horne@hotmail.co.uk>
2024-05-09 11:52:16 +0100

    softwareenginner done

--- classes/SoftwareEngineer.js
+++ classes/SoftwareEngineer.js
@@ -1,4 +1,20 @@
-class SoftwareEngineer {
+const { Employees } = require("./Employees");
+
+class SoftwareEngineer extends Employees{
+    #programmingLanguages
+    constructor(programmingLanguages = []){
+        this.#programmingLanguages = programmingLanguages
+    }
+
+    getProgrammingLanguages(){
+        return this.#programmingLanguages
+    }
+
+    setProgrammingLanguages(language){
+        if (language === undefined){throw new Error("You must include a language")}
+        else if (language === null){throw new Error("You must include a language")}
+        else this.#programmingLanguages.push(language)
+    }
 
 
 
